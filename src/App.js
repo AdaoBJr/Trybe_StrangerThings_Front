9abab0cef6d484e4ff4c8ad7e371040bec8f5f0e
tonpasso <wellpasso@gmail.com>
2021-09-29 20:10:27 -0300

    retirando deesenvolvimento do pd

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const ambiente = process.env.REACT_APP_AMBIENTE || 'deu erro';
-
 function App() {
   return (
     <div className="App">
-      { `<h2>${ambiente}</h2>` }
       <StrangerThings />
     </div>
   );
