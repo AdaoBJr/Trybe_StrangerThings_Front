844b08278b9d63159f8b6c61a9b27861c41f1cc6
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-09-29 20:47:56 -0300

    teste cypress

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      {console.log(process.env.REACT_APP_DEVELOPMENT)}
       { process.env.REACT_APP_DEVELOPMENT === 'true'
-        ? <h1>Em desenvolvimento</h1> : <StrangerThings /> }
+        && <h1>Em desenvolvimento</h1> }
+      <StrangerThings />
     </div>
   );
 }
