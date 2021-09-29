b5171b1c91946cb5e1bab5b631d22ab1b41e8139
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-09-29 19:48:44 -0300

    adicionando condiçao

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      {process.env.DEVELOPMENT === 'true'
+      { process.env.DEVELOPMENT === 'true'
         ? <h1>Em desenvolvimento</h1> : <StrangerThings /> }
     </div>
   );
