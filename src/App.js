7b349a135e8767cef9200bf843097c352aae05ce
Alessandra <alessandra.rezende@sambatech.com.br>
2021-09-29 20:16:24 -0300

    Bonus

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
-      <h2>Em desenvolvimento</h2>
+      {process.env.REACT_APP_DEPLOYMENT && <h2>Em desenvolvimento</h2>}
       <StrangerThings />
     </div>
   );
