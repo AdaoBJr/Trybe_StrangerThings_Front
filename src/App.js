16ccb0041fa2436215f75e00039d1b791d0e96a7
Caio <thehaseol@gmail.com>
2021-09-29 20:44:27 -0300

    fix lint v2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
-    <div className='App'>
-      {process.env.REACT_APP_DEV_MODE === 'true' && <h2>Em desenvolvimento</h2>}
+    <div className="App">
+      { process.env.REACT_APP_DEV_MODE === 'true' && <h2>Em desenvolvimento</h2> }
       <StrangerThings />
     </div>
   );
