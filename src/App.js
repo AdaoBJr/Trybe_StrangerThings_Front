553242fcccf36d183e293de1eee7b7fe58b2026a
Paulo Xavier <pxavierf@gmail.com>
2021-09-29 19:13:47 -0300

    mudando para ambiente de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import 'dotenv';
 
 import StrangerThings from './components/StrangerThings';
+
+const isDevelopment = process.env.REACT_APP_AMBIENTE === 'development';
 
 function App() {
   return (
     <div className="App">
+      { isDevelopment && <h1>Em desenvolvimento</h1> }
       <StrangerThings />
     </div>
   );
