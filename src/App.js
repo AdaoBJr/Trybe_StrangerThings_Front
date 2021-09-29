e2fba6a08510babb24ddb47a2f0d0e49c1fc3085
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-09-29 20:09:14 -0300

    console logs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      {console.log(process.env.DEVELOPMENT)}
+      {console.log(typeof (process.env.DEVELOPMENT))}
       { process.env.DEVELOPMENT === 'true'
         ? <h1>Em desenvolvimento</h1> : <StrangerThings /> }
     </div>
