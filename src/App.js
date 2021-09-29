6161e3b79700ebeb575df2f1af46a6a4fe9516eb
tonpasso <wellpasso@gmail.com>
2021-09-29 19:16:32 -0300

    deveplopment

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const ambiente = process.env.REACT_APP_AMBIENTE || 'deu erro';
+
 function App() {
   return (
     <div className="App">
+      { `${ambiente}` }
       <StrangerThings />
     </div>
   );
