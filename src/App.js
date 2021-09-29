4f7fc45b66865aed505d2f12431bdeba4ae62ce0
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-09-29 20:15:16 -0300

    env variavel

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      {console.log(process.env.DEVELOPMENT)}
-      {console.log(typeof (process.env.DEVELOPMENT))}
-      { process.env.DEVELOPMENT === 'true'
+      {console.log(process.env.REACT_APP_DEVELOPMENT)}
+      {console.log(typeof (process.env.REACT_APP_DEVELOPMENT))}
+      { process.env.REACT_APP_DEVELOPMENT === 'true'
         ? <h1>Em desenvolvimento</h1> : <StrangerThings /> }
     </div>
   );
