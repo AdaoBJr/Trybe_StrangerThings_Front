e388c16dbfa712bf76453378adf4fbf7d23d9b97
Caio <thehaseol@gmail.com>
2021-09-29 20:21:01 -0300

    Iniciando parte 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+            { process.env.REACT_APP_DEV_MODE === 'true' && <h2>Em desenvolvimento</h2> }
       <StrangerThings />
     </div>
   );
