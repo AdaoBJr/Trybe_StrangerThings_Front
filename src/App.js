c9829b875038c897528a8a02ded5a593123960a2
riquee <strikedv@hotmail.com>
2021-09-29 07:33:13 -0300

    req 8 e 9 finalizados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
+      { process.env.REACT_APP_DEVELOPMENT === 'true' && <h2>Em desenvolvimento</h2>}
       <StrangerThings />
     </div>
   );
