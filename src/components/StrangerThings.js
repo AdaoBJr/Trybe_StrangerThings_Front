ac501fb47c42b551c4d03bbdea848bb89662d22f
brunobastosfer <brunobastos@edu.unifor.br>
2021-09-29 18:54:17 -0300

    ajustando variáveis de ambiente e adicionando ao componente StrangerThings

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,18 +2,30 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
+require('dotenv').config();
+
+const magicNumber = 30000;
+
+const REACT_APP_HAWKINS_URL = process.env.REACT_APP_HAWKINS_URL || 'https://brunobastosfer-bk.herokuapp.com/';
+
+const REACT_APP_HAWKINS_TIMEOUT = process.env.REACT_APP_HAWKINS_TIMEOUT || magicNumber;
+
+const REACT_APP_UPSIDEDOWN_URL = process.env.REACT_APP_UPSIDEDOWN_URL || 'https://brunobastosfer-bd.herokuapp.com/';
+const REACT_APP_UPSIDEDOWN_TIMEOUT = process.env
+  .REACT_APP_UPSIDEDOWN_TIMEOUT || magicNumber;
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: REACT_APP_HAWKINS_URL,
+  timeout: REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: REACT_APP_UPSIDEDOWN_URL,
+  timeout: REACT_APP_UPSIDEDOWN_URL,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
