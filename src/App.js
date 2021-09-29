5fb5deb1eb7ede586c1ced84b6fdb287bc370c4d
tonpasso <wellpasso@gmail.com>
2021-09-29 20:19:21 -0300

    add development

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import 'dotenv';
 
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
+      { process.env.REACT_APP_DEVELOPMENT && <h2>Em desenvolvimento</h2> }
       <StrangerThings />
     </div>
   );
