872d4bb8e609c77fe7a01840cd5699eb24a677e2
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-30 19:04:01 -0300

    subistituindo tag div por tag h1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
+  const status = process.env.REACT_APP_STATUS;
   return (
     <div className="App">
-      <div>Em desenvolvimento</div>
+      <h1>{status}</h1>
       <StrangerThings />
     </div>
   );
