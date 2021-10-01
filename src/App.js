fa07b1c939397387a4f7cf5d52baab7dc20b3ed2
CIGR2021 <appencigr@gmail.com>
2021-10-01 18:15:28 -0300

    adicionei e a variável de ambiente no app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
+require('dotenv').config();
+
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
-      <h2>Em desenvolvimento</h2>
+      {process.env.REACT_APP_DEPLOYMENT && <h2>Em desenvolvimento</h2>}
       <StrangerThings />
     </div>
   );
