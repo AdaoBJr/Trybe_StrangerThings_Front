a1366ac59a1df295a3ea376150ea5b405c5832c8
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-09-30 22:33:51 -0300

    App desenvolvimento

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      { process.env.APP_DESENVOLVIMENTO === 'true' && <h3>Em desenvolvimento</h3>}
       <StrangerThings />
     </div>
   );
