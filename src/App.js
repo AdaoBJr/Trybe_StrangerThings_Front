cb48769b902c1fbfe97c6412d8577c876c1d190b
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-02 18:46:35 -0400

    front end

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
+      {process.env.REACT_APP_DEPLOYMENT === 'true' ? <h2>Em desenvolvimento</h2> : null}
       <StrangerThings />
     </div>
   );
