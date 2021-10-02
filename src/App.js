40dbc9e597407d70e3141b87d75d961e9ac25cc3
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-02 19:38:31 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 function App() {
   return (
     <div className="App">
-
       {process.env.REACT_APP_DEPLOYMENT === 'true' ? <h2>Em desenvolvimento</h2> : null}
       <StrangerThings />
     </div>
