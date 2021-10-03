cf418be0c1b3d376de9eef0ac52593ff7989537a
antonioarieiro <sfgfffs@gmail.com>
2021-10-02 22:38:25 -0300

    trabalho feito na t9

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
+import TagComponent from './components/TagComponent';
+
+require('dotenv').config();
+
+const devAmb = process.env.REACT_APP_TAG_DEV === 'true';
 
 function App() {
   return (
     <div className="App">
+      { devAmb && <TagComponent /> }
       <StrangerThings />
     </div>
   );
