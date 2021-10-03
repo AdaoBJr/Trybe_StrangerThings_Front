abbdaf9b2ee55e7cb4ebd96199abf3318ae497a2
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-10-03 12:48:51 -0300

    Requisitos 1

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,13 +7,13 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
@@ -104,6 +104,7 @@ class StrangerThings extends React.Component {
   }
 
   render() {
+    console.log(process.env.REACT_APP_HAWKINS_UR);
     const {
       hereIsTheUpsideDownWorld, characterName, characters, page,
     } = this.state;
