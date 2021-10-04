f83cf8127531b91bfb93e4ad014aa6efb1f3a905
Daniel <dsouzaborini@gmail.com>
2021-10-04 04:07:40 -0300

    config communication to api

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const DEFAULT_TIMEOUT = 30000;
+const { REACT_APP_HAWKINS_URL, REACT_APP_HAWKINS_TIMEOUT } = process.env;
+
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
+  constructor({ url = REACT_APP_HAWKINS_URL, timeout = REACT_APP_HAWKINS_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
