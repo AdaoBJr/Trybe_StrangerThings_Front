b029a0abee5e8deafaa58160488d754973a78fe1
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-10-04 16:16:35 -0300

    ajuste variavel ambiente

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const APP_DESENVOLVIMENTO = process.env.APP_DESENVOLVIMENTO || false;
-
 function App() {
   return (
     <div className="App">
       { REACT_APP_DESENVOLVIMENTO === 'true' && <h3>Em desenvolvimento</h3> }
-      { console.log(APP_DESENVOLVIMENTO) }
       <StrangerThings />
     </div>
   );
