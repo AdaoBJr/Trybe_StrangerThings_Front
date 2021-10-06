01ee649f69847c6245caf659abc1018b045c0438
arthur menezes <arthurmcorrea282@gmail.com>
2021-10-06 09:58:19 -0300

    requisito 7

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -9,7 +9,6 @@ const { REACT_APP_HAWKINS_URL,
   REACT_APP_UPSIDEDOWN_URL,
   REACT_APP_UPSIDEDOWN_TIMEOUT,
 } = process.env;
-
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
