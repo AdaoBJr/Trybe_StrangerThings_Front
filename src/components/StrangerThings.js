6c207688ac34eaf664cf00c989e5843cf578bf8a
Pedro Toselli <phtoselli@gmail.com>
2021-10-08 14:56:16 -0300

    Requisito 1 - Adicionando variaveis de ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,12 +7,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
+  url: 'https://pedro-toselli-bk.herokuapp.com/',
   timeout: 30000,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
+  url: 'https://pedro-toselli-bd.herokuapp.com/',
   timeout: 30000,
 };
 
