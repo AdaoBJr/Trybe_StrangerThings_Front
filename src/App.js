c5b8405d1036ea878c7280c4c318f3c817bb7aa3
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-10-11 18:54:10 -0300

    Mudança do nome da variavel

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      { process.env.DEVELOPMENT === 'ativo' && <h1>Em desenvolvimento</h1>}
+      { process.env.REACT_APP_DEVELOPMENT === 'ativo' && <h1>Em desenvolvimento</h1>}
       <StrangerThings />
     </div>
   );
