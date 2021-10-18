c5231b6545b3bc856ab825589468cff9780aff0d
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-10-18 18:08:45 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-require('dotenv').config();
-
 function App() {
   return (
     <div className="App">
-      <h3>Em desenvolvimento</h3>
+      <h3 hidden>Em desenvolvimento</h3>
       <StrangerThings />
     </div>
   );
