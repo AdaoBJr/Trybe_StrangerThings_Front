e68eb03ce4e6c430df073ab053c1a680e9d5b3db
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-10-18 18:38:27 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      <h3>Em desenvolvimento</h3>
+      { process.env.REACT_APP_DEVELOPMENT === 'true' && <h3>Em desenvolvimento</h3> }
       <StrangerThings />
     </div>
   );
