cd2f3b278211450547e4a3601544e5555f3fbd77
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-10-22 19:55:24 -0300

    Alteraçao do local da tag em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import StrangerThings from './components/StrangerThings';
 
 require('dotenv').config();
 
-const server = process.env.SERVER_ENV;
-
 function App() {
   return (
     <div className="App">
-      { server === 'development' && <h2>Em desenvolvimento</h2>}
+      { process.env.SERVER_ENV === 'development' && <h2>Em desenvolvimento</h2>}
       <StrangerThings />
     </div>
   );
