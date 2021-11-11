a42137294698e278450ba4634d00a0d7c78e0596
brunobastosfer <brunobastos@edu.unifor.br>
2021-11-10 21:00:28 -0300

    ajuste

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
-import 'dotenv/config'
+import 'dotenv/config';
 
 // require('dotenv').config();
 
