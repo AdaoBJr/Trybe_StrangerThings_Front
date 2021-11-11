b9bdadf85e688d417942b60d43b21f5f86309d54
v-icto-r <victor130496@gmail.com>
2021-11-11 13:01:24 -0300

    Em desenvolvimento

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -123,6 +123,7 @@ class StrangerThings extends React.Component {
         )}` }
       >
         <div className="content strangerfy">
+          <div>Em desenvolvimento</div>
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               Mudar de Realidade
