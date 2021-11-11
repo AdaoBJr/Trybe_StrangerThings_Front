a69540122a2569893a6d51ac09da39b05e3edf9f
walterlbf <walter.lbf.f@gmail.com>
2021-11-11 07:24:47 -0400

    corrigindo os erros de lint dessa caralha

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -111,14 +111,16 @@ class StrangerThings extends React.Component {
     } = this.state;
     return (
       <div
-        className={`reality ${getRealityClass(
-          hereIsTheUpsideDownWorld,
-        )}`}
+        className={
+          `reality ${getRealityClass(
+            hereIsTheUpsideDownWorld,
+          )}`
+        }
       >
         {process.env.REACT_APP_DEVELOPMENT === true && <h1>Em desenvolvimento</h1>}
         <div className="content strangerfy">
           <div className="change-reality">
-            <button type="button" onClick={this.changeRealityClick}>
+            <button type="button" onClick={ this.changeRealityClick }>
               {' '}
               Mudar de Realidade
             </button>
@@ -127,14 +129,16 @@ class StrangerThings extends React.Component {
           <div>
             <input
               placeholder="Nome do Personagem"
-              onChange={this.handleInput}
-              value={characterName}
+              onChange={
+                this.handleInput
+              }
+              value={ characterName }
             />
-            <button type="button" onClick={this.searchClick}>Pesquisar</button>
+            <button type="button" onClick={ this.searchClick }>Pesquisar</button>
           </div>
 
           <div>
-            <Table characters={characters} />
+            <Table characters={ characters } />
           </div>
 
           <div>
@@ -144,8 +148,8 @@ class StrangerThings extends React.Component {
             </p>
           </div>
           <div>
-            <button type="button" onClick={this.previousPage}>Anterior</button>
-            <button type="button" onClick={this.nextPage}>Próximo</button>
+            <button type="button" onClick={ this.previousPage }>Anterior</button>
+            <button type="button" onClick={ this.nextPage }>Próximo</button>
           </div>
         </div>
       </div>
